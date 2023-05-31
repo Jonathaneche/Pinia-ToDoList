f97153fe3228b8b416ad07f54999b7ae972f60b2
jonathaneche <jonathan.eche@gmail.com>
2023-05-31 17:20:39 +0200

    uso del store en el componente app.vue y pasando cada task por props al componente TaskDetails

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,35 @@
     <!-- heading -->
     <header>
       <img src="./assets/pinia-logo.svg" alt="">
-      <h1>Pinia Tasks</h1>
+      <h1>{{ taskStore.name }}</h1>
     </header>
+
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+      <TaskDetails :task="task"/>
+    </div>
+    </div>
+
   </main>
-  
+
 </template>
+
+
+
+<script>
+import TaskDetails from './components/TaskDetails.vue'
+import { useTaskStore } from './store/TaskStore';
+
+export default {
+  components: { TaskDetails},
+  setup() {
+    const taskStore = useTaskStore()
+
+    return {
+      taskStore
+    }
+  }
+}
+
+</script>
---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="task">
+        <h3>{{ task.title }}</h3>
+        <div class="icons">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['task']
+    }
+</script>
