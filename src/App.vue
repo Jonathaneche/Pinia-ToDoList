c52faeab44565c111cd7754ffc4ff0fc7a79b435
jonathaneche <jonathan.eche@gmail.com>
2023-05-31 21:24:41 +0200

    Se puede usar storeToRefs para no tener que escribir el nombre del store al momento de utilizar un state o getter

--- src/App.vue
+++ src/App.vue
@@ -20,23 +20,28 @@
 
     <!-- loading -->
 
-    <div class="loading" v-if="taskStore.loading">Loading tasks...</div>
+    <div class="loading" v-if="loading">Loading tasks...</div>
 
     <!--All tasks list -->
     <div class="task-list" v-if="filter === 'all'">
-      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
-      <div v-for="task in taskStore.tasks" :key="task.id"> <!-- Aqui estamos usando el estado tasks que contiene todas las tareas-->
+      <p>You have {{ totalCount }} tasks left to do</p>
+      <div v-for="task in tasks" :key="task.id"> <!-- Aqui estamos usando el estado tasks que contiene todas las tareas-->
       <TaskDetails :task="task"/>
     </div>
     </div>
 
         <!-- favs tasks list -->
       <div class="task-list" v-if="filter === 'favs'">
-        <p>You have {{ taskStore.favCount }} fav tasks left to do</p>
-        <div v-for="task in taskStore.favs" :key="task.id"> <!-- Aqui estamos usando el getter favs que contiene las tareas favoritas-->
+        <p>You have {{ favCount }} fav tasks left to do</p>
+        <div v-for="task in favs" :key="task.id"> <!-- Aqui estamos usando el getter favs que contiene las tareas favoritas-->
         <TaskDetails :task="task"/>
       </div>
       </div>
+
+      <!-- <button @click="taskStore.$reset">Reset state </button>         -->
+      <!-- Se puede resetear los estados del store usando $reset, descomentar el boton y probar-->
+
+
 
   </main>
 
@@ -45,6 +50,7 @@
 
 
 <script>
+import { storeToRefs } from 'pinia';
 import TaskDetails from './components/TaskDetails.vue'
 import TaskForm from './components/TaskForm.vue'
 import { useTaskStore } from './store/TaskStore';
@@ -55,6 +61,11 @@ export default {
   setup() {
     const taskStore = useTaskStore()
 
+    // store to refs, Creates an object of references with all the state, getters, and plugin-added state properties of the store. Similar to toRefs() but specifically designed for Pinia stores so methods and non reactive properties are completely ignored. The param to pass is a store — store to extract the refs from
+    // Para llamar a las acciones se debe realizar de forma habitual ejemplo: taskStore.addTask
+
+    const {tasks, loading, favs, totalCount, favCount} = storeToRefs(taskStore)
+
     //fetch tasks
 
     taskStore.getTasks()
@@ -62,7 +73,7 @@ export default {
     const filter = ref('all')
 
     return {
-      taskStore, filter
+      taskStore, filter , tasks, loading, favs, totalCount, favCount
     }
   }
 }
